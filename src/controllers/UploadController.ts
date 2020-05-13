3bd143ebec0b94d085c373fedc231310f75d91f4
Alifilho <alissonoliveiram@gmail.com>
2020-05-12 21:25:52 -0300

    Google Cloud auth

--- src/controllers/UploadController.ts
+++ src/controllers/UploadController.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from "express";
+import { ImageAnnotatorClient } from "@google-cloud/vision";
 
 import sendImage from "../services/api";
 
 export default {
   async store(req: Request, res: Response) {
-    const detections = await sendImage(req.body.base64);
-    res.json(detections);
+    const { key, email } = req.body;
+    const { type } = req.headers;
+    const client = new ImageAnnotatorClient({
+      credentials: {
+        private_key: key,
+        client_email: email,
+      },
+    });
+    console.log(type);
+    res.json({ message: "OK" });
   },
 };
